259e4affc65947f6b6219a4b54a26cc796b2659e
aakash.t <aakash.t@MNSPL-MAC-130.local>
2019-08-07 22:27:10 +0530

    first commit

---
+++ src/components/Services.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import Title from './Title'
+import { FaCocktail,FaHiking,FaShuttleVan,FaBeer } from 'react-icons/fa';
+
+export default class Services extends Component {
+
+    state = {
+        services:[
+            {
+                icon:<FaCocktail />,
+                title:'free cocktails',
+                info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, porro non. Vitae voluptas rerum doloribus modi dignissimos cumque aperiam exercitationem quas vero, obcaecati laudantium eius beatae tempore doloremque dolor culpa?'
+            },
+            {
+                icon:<FaHiking />,
+                title:'Endless hiking',
+                info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, porro non. Vitae voluptas rerum doloribus modi dignissimos cumque aperiam exercitationem quas vero, obcaecati laudantium eius beatae tempore doloremque dolor culpa?'
+            },
+            {
+                icon:<FaShuttleVan />,
+                title:'free shuttle',
+                info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, porro non. Vitae voluptas rerum doloribus modi dignissimos cumque aperiam exercitationem quas vero, obcaecati laudantium eius beatae tempore doloremque dolor culpa?'
+            },
+            {
+                icon:<FaBeer />,
+                title:'Strongest Beer',
+                info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, porro non. Vitae voluptas rerum doloribus modi dignissimos cumque aperiam exercitationem quas vero, obcaecati laudantium eius beatae tempore doloremque dolor culpa?'
+            }
+        ]
+    }
+
+    render() {
+        return (
+            <section className="services">
+               <Title title="services" />
+               <div className="services-center">
+                   {this.state.services.map((item,index) => {
+                       return (
+                        <action key={index} className="service">
+                            <span>{item.icon}</span>
+                            <h6>{item.title}</h6>
+                            <p>{item.info}</p>
+                        </action>
+                       )
+                   })}
+               </div>
+            </section>
+        )
+    }
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import Hero from '../components/Hero'
+import Banner from '../components/Banner';
+import Services from '../components/Services';
+
+export default function Home() {
+    return (
+        <>
+            <Hero>
+                <Banner title="luxurios rooms" subtitle="deluxe rooms starting at $2400">
+                    <Link to="/rooms" className="btn-primary">our rooms</Link>
+                </Banner>
+            </Hero>
+            <Services />
+        </>
+    )
+}
